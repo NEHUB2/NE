c0954baa5a8eeb9a1f757c38a8cf88bff723c449
NEHUB2 <3772971322@qq.com>
2025-09-20 23:37:49 +0800

    Add files via upload

--- id设备.lua
+++ id设备.lua
@@ -10,6 +10,8 @@ return {
 
     ["2bd4d2ad-ca9e-4a79-be53-09eb75a794d4"] = "KEY_f4664976773sjjdjdie",
     
+    ["cc3b9731-087d-44cf-a350-4a723fc3caec"] = "V3X4KFG4369V845UIE2",
+    
     ["19fb2d22-8c72-4816-992b-d7612d2eaecd"] = "KEY_a8d3e7f2g5h9j1k4m6n",
     
     ["91f2f674-e9e9-4994-82dc-1920ed7f2145"] = "KEY_b9c4d6e8f1g3h5j7k2m",
